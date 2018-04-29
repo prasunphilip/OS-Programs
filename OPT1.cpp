eb1c5448855ba0523ce4d1f5bbdb43bbb121a3d7
prasunphilip <32845318+prasunphilip@users.noreply.github.com>
2018-04-29 18:31:51 +0530

    Add files via upload

---
+++ OPT1.cpp
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int f,a[100],b[100]={0},i,n,k,j,t=0,h=0,m=0,l=0,p,u=0,e=0,q=0,x=0;
+	printf("Enter the number of frames\n");
+	scanf("%d",&f);
+	e=f;
+	printf("Enter the number of pages\n");
+	scanf("%d",&n);
+	printf("Enter the pages\n");
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+		
+	printf("\n");
+	for(i=0;i<n;i++)
+	{
+		if(i<f||u<f)
+		{
+			for(j=0;j<f;j++)
+			{
+				if(a[i]==b[j])
+				{
+					t++;
+					h++;
+					break;
+				}
+			}
+			if(t==0)
+			{
+				m++;
+				b[l]=a[i];
+				u++;
+				l++;
+			}
+			t=0;
+		}
+		else
+		{
+			for(j=0;j<f;j++)
+			{
+				if(a[i]==b[j])
+				{
+					t++;
+					h++;
+					break;
+				}
+			}
+			if(t==0)
+			{
+				m++;
+				for(p=0;p<f;p++)
+				{
+					for(j=i+1;j<n;j++)
+					{
+						if(b[p]!=a[j] && x==0)
+						{
+							q++;
+						}
+						else if(b[p]==a[j])
+						{
+							e=p;
+							x++;
+							q=0;
+						}
+					}
+					if(q!=0)
+					{
+						b[p]=a[i];
+						break;
+					}
+					x=0;
+				}
+				if(q==0)
+					b[e]=a[i];
+			}
+			t=0;
+		}
+		if(u<f)
+		{
+			for(p=0;p<u;p++)
+				printf("%d ",b[p]);
+		}
+		else
+		{
+			for(p=0;p<f;p++)
+				printf("%d ",b[p]);
+		}
+		printf("\n");
+	}
+
+	printf("No of Hits : %d\n",h);
+	printf("No of Miss : %d",m);
+	return 0;
+}
---
+++ RR1.cpp
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,b[100],w[100],t[100],i,j,avgw=0,avgt=0,p[100],k=0,g,q,l[100],v=0,z[100];
+	printf("Enter the number of processes\n");
+	scanf("%d",&n);
+	k=n-1;
+	printf("Enter the burst time of the processes\n");
+	for(i=0;i<n;i++)
+	{
+		p[i]=i+1;
+		scanf("%d",&b[i]);
+	}
+	printf("Enter the time quantum\n");
+	scanf("%d",&q);
+	printf("\n");
+	w[0]=0;
+	for(i=0;i<=k;i++)
+	{
+		if(b[i]>q)
+		{
+			k++;
+			w[i+1]=w[i]+q;
+			b[k]=b[i]-q;
+			p[k]=p[i];
+		}
+		else
+			w[i+1]=w[i]+b[i];
+	}
+	
+	for(g=0;g<n;g++)
+	{
+		for(i=0;i<=k;i++)
+		{
+			if((g+1)==p[i])
+			{
+				v++;
+				z[g]=v;
+			}
+		}
+		v=0;
+	}
+
+	for(g=0;g<n;g++)
+	{
+		for(i=0;i<=k;i++)
+		{
+			if((g+1)==p[i] && z[g]!=0)
+			{
+				if(z[g]!=1)
+				{
+					l[g]+=w[i];
+					l[g]-=w[i+1];
+					z[g]-=1;
+				}
+				else 
+					l[g]+=w[i];
+			}
+		}
+	}
+	printf("\n");
+	for(i=0;i<n;i++)
+	{
+		printf("Waiting time for Process %d is %d\n",i+1,l[i]);
+		avgw+=l[i];
+	}
+	printf("Average Waiting time is %f\n",(float)avgw/n);
+	printf("\n");
+	for(i=0;i<n;i++)
+	{
+		t[i]=l[i]+b[i];
+		printf("Turn-around time for Process %d is %d\n",i+1,t[i]);
+		avgt+=t[i];
+	}
+	printf("Average Turn-around time is %f\n",(float)avgt/n);
+	printf("\nGantt Chart\n");
+	for(i=0;i<=k;i++)
+		printf("|   P%d\t", p[i]);
+	printf("|");
+	printf("\n");
+	for(i=0;i<=k+1;i++)
+		printf("%d\t", w[i]);
+	return 0;
+}
